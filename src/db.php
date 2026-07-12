56ba06afa3eb26055790d89b4fa2c1579b6ebc9d
ali1140 <ali1140@users.noreply.github.com>
2026-07-12 15:52:13 +0700

    Fix db.php to return JSON on connection error

--- src/db.php
+++ src/db.php
@@ -4,10 +4,18 @@ $username = getenv('DB_USER') ?: "root";
 $password = getenv('DB_PASSWORD') !== false ? getenv('DB_PASSWORD') : "";            
 $dbname = getenv('DB_NAME') ?: "attendance_system"; 
 
+// Disable default PHP error reporting for mysqli to prevent HTML warnings from breaking JSON
+mysqli_report(MYSQLI_REPORT_OFF);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = @new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    header('Content-Type: application/json');
+    echo json_encode([
+        'success' => false,
+        'message' => 'Koneksi database gagal: ' . $conn->connect_error,
+        'data' => []
+    ]);
+    exit();
 }
 ?>
